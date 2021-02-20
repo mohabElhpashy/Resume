327ce4151ac83b423c03ee00ded0e868ed3ad744
mohabelhbashy <mohab.hafez332@gmail.com>
2021-02-20 20:48:14 +0200

    media

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,40 +1,39 @@
 import "./Navbar.css";
 
+import { IoMdReorder } from "react-icons/io";
+
 function Nnavbar() {
   return (
-    <div className="main">
-      <h2>MOhaB</h2>
-      <div className="links">
-        <ul>
-          <li className="active">
-            <a href="mohab">Home</a>
-          </li>
-          <li>
-            <a href="mohab">About</a>
-          </li>
-          <li>
-            <a href="mohab">Portfolio</a>
-          </li>
-          <li>
-            <a href="mohab">SKILLS</a>
-          </li>{" "}
-          <li>
-            <a href="mohab">CONTACT</a>
-          </li>
-        </ul>
+    <div>
+      <div className="main">
+        <h2>MOhaB</h2>
+        <div className="links">
+          <ul>
+            <li className="active">
+              <a href="mohab">Home</a>
+            </li>
+            <li>
+              <a href="mohab">About</a>
+            </li>
+            <li>
+              <a href="mohab">Portfolio</a>
+            </li>
+            <li>
+              <a href="mohab">SKILLS</a>
+            </li>{" "}
+            <li>
+              <a href="mohab">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+        <div class="fh5co-footer">
+          <p>
+            <small>© 2020 badr. All Rights Reserved</small>
+          </p>
+        </div>
       </div>
-      <div class="fh5co-footer">
-        <p>
-          <small>
-            © 2020 badr. All Rights Reserved
-            {/* <span>
-              Designed by <a href="mohab.hafez332@gmail.com">MOhaB</a>
-            </span>
-            <span>
-              Freelancer: <a>Front-end</a>
-            </span> */}
-          </small>
-        </p>
+      <div className="threelines">
+        <IoMdReorder size={50} color="white" />
       </div>
     </div>
   );
