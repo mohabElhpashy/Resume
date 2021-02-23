546b8cfd73d17478fa9033ab8de9fbd9496818c2
mohabelhbashy <mohab.hafez332@gmail.com>
2021-02-23 20:33:43 +0200

    add boot starp

--- src/Components/Welcome/Welcome_page.js
+++ src/Components/Welcome/Welcome_page.js
@@ -2,7 +2,11 @@ import "./Welcome_page.css";
 import { Component } from "react";
 import img1 from "../../img/diego-ph-5LOhydOtTKU-unsplash.jpg";
 import img2 from "../../img/vincent-guth-uhoILl3HUZM-unsplash.jpg";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { FcSettings, FcSearch, FcLineChart } from "react-icons/fc";
+import { FaRegPaperPlane } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Welcome extends Component {
   state = {
@@ -22,7 +26,7 @@ class Welcome extends Component {
   }
   render() {
     return (
-      <div className="container">
+      <div>
         <img
           className="Welcome"
           alt="sora"
@@ -34,17 +38,84 @@ class Welcome extends Component {
             <h2>WEB DEVElOPER</h2>
             <Button
               style={{
-                width: "150px",
+                width: "170px",
                 height: "50px",
                 letterSpacing: "3px",
-                fontFamily: "URW Chancery L, cursive	",
+                fontFamily: "Courier, monospace		",
               }}
               variant="light"
             >
               DownLOAD CV
+              <AiOutlineArrowDown />
             </Button>
           </div>
         </div>
+        <Container>
+          <Row>
+            <Col>
+              <div className="main_div">
+                <div className="fh5co-icon">
+                  <FcSettings size={80} enableBackground="gray" color="red" />
+                </div>
+                <div class="fh5co-text">
+                  <h3>Strategy</h3>
+                  <p>
+                    Working on creating a creative and responsive project and
+                    making some animation in my projects so that it has realism.{" "}
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="main_div">
+                <div class="fh5co-icon">
+                  <FcSearch size={80} />
+                </div>
+                <div class="fh5co-text">
+                  <h3>Explore</h3>
+                  <p>
+                    I always strive to learn and acquire more and follow what
+                    the market is going through in terms of updates and changes
+                    to programming languages.{" "}
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <div className="main_div">
+                <div class="fh5co-icon">
+                  <FaRegPaperPlane size={50} />
+                </div>
+                <div class="fh5co-text">
+                  <h3>Direction</h3>
+                  <p>
+                    I receive the work from UI&amp;UX designer and work on
+                    coding psd file in an organized way, with the addition of
+                    some additions that I like.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="main_div">
+                <div class="fh5co-icon">
+                  <FcLineChart size={80} />
+                </div>
+                <div class="fh5co-text">
+                  <h3>Expertise</h3>
+                  <p>
+                    Experienced Front-end web development as Front-end developer
+                    With history of working in the information technology web
+                    design &amp; development.{" "}
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
