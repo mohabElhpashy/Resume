de309890eeaeb0eda06a5a53717745e73b2a47ad
mohabelhbashy <mohab.hafez332@gmail.com>
2021-03-15 15:43:51 +0200

    edit_v_1

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -23,31 +23,58 @@ class Skills extends Component {
   };
   render() {
     return (
+      // <Container>
+      //   <Row className="header_bar">
+      //     <Col className="header_bar">
+      //       <h1>Portfoli</h1>
+      //       <div className="linee"></div>
+      //     </Col>
+      //     <Col data-aos="fade-right" lg={6} md={6} sm={6} xs={12}>
+      //       <div className="projects">
+      //         <img
+      //           onClick={this.goto1}
+      //           style={{ width: "401.49px" }}
+      //           src={Capture}
+      //           alt="project"
+      //         />
+      //       </div>
+      //     </Col>
+      //     <Col data-aos="fade-left" lg={6} md={6} sm={6} xs={12}>
+      //       <div className="projects">
+      //         <img
+      //           onClick={this.goto2}
+      //           style={{ width: "401.49px" }}
+      //           src={Musiq}
+      //           alt="project"
+      //         />
+      //       </div>
+      //     </Col>
+      //   </Row>
+      // </Container>
       <Container>
-        <Row className="header_bar">
-          <Col className="header_bar">
-            <h1>Portfoli</h1>
-            <div className="linee"></div>
-          </Col>
-          <Col data-aos="fade-right" lg={6} md={6} sm={6} xs={12}>
-            <div className="projects">
-              <img
-                onClick={this.goto1}
-                style={{ width: "401.49px" }}
-                src={Capture}
-                alt="project"
-              />
+        <Row>
+          <Col lg={6} xs={12}>
+            <div className="f_box">
+              <div className="f_box1">
+                <h1>test</h1>
+                <p>mohab is web development and i need more jops</p>
+              </div>
+              <div className="f_box2">
+                <h1>test</h1>
+                <p>mohab is web development and i need more jops</p>
+              </div>
+              <div className="f_box3">
+                <h1>test</h1>
+                <p>mohab is web development and i need more jops</p>
+              </div>
+              {/* <div className="f_box4">
+                <h1>test</h1>
+                <p>mohab is web development and i need more jops</p>
+              </div>{" "} */}
             </div>
           </Col>
-          <Col data-aos="fade-left" lg={6} md={6} sm={6} xs={12}>
-            <div className="projects">
-              <img
-                onClick={this.goto2}
-                style={{ width: "401.49px" }}
-                src={Musiq}
-                alt="project"
-              />
-            </div>
+          <Col lg={6}>
+            <div className="box">2</div>
           </Col>
         </Row>
       </Container>
